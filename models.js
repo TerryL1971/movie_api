51fe3ecfaeb27076ed3b7475cc07a54c977dae2e
TerryL1971 <tcl71771@yahoo.com>
2021-06-29 15:12:30 +0200

    Update models.js

--- models.js
+++ models.js
@@ -1,41 +1,51 @@
 const mongoose = require('mongoose');
-
 const bcrypt = require('bcrypt');
 
-let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Descripton: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
 });
 
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+// generates password with 10 characters
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
+// checks if password exists in database (instance method)
 userSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
-let movieSchema = mongoose.Schema ({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
-  Genre: {
-    Name: String,
-    Description: String
-  },
-  Director: {
-    Name: String,
-    Bio: String
-  },
-  Actos: [String],
-  ImagePath: String,
-  Featured: Boolean
-});
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: {type: String, required: true},
+    });
 
+// creates models that use the schemas defined
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
 
+// imports models into index.js file
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
